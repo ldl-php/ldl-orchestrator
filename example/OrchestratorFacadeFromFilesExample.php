1659ab80373249908749c86cb99f34e163597811
Federico Stange <jpfstange@gmail.com>
2022-06-09 15:15:03 +0000

    feature/1202421580425638 - Facade method to build from single files

--- example/OrchestratorFacadeFromFilesExample.php
+++ example/OrchestratorFacadeFromFilesExample.php
@@ -5,24 +5,25 @@ declare(strict_types=1);
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/Build/autoload.php';
 
-use LDL\File\Collection\DirectoryCollection;
-use LDL\File\Directory;
 use LDL\File\Helper\FilePathHelper;
-use LDL\Orchestrator\Builder\OrchestratorBuilder;
-use LDL\Orchestrator\Collection\OrchestratorCollection;
 use LDL\Orchestrator\Facade\OrchestratorFacade;
 use LDL\Orchestrator\Loader\OrchestratorLoader;
-use LDL\Type\Collection\Types\String\StringCollection;
+
+$fromFiles = OrchestratorFacade::fromFiles(
+    [
+        FilePathHelper::createAbsolutePath(__DIR__, 'Build', 'Application', 'Admin', 'services.xml'),
+        FilePathHelper::createAbsolutePath(__DIR__, 'Build', 'Application', 'User', 'services.xml'),
+        FilePathHelper::createAbsolutePath(__DIR__, 'Build', 'Framework', 'Services', 'Mailer', 'services.xml'),
+        FilePathHelper::createAbsolutePath(__DIR__, 'Build', 'Framework', 'Services', 'Template', 'services.xml'),
+    ],
+    [
+        FilePathHelper::createAbsolutePath(__DIR__, 'Build', 'Application', 'Admin', '.env'),
+        FilePathHelper::createAbsolutePath(__DIR__, 'Build', 'Application', 'User', '.env'),
+    ]
+);
 
 $loader = new OrchestratorLoader();
-
-$orchestrators = new OrchestratorCollection([
-    OrchestratorFacade::fromDirectory(new DirectoryCollection([
-        new Directory(FilePathHelper::createAbsolutePath(__DIR__, 'Build')),
-    ]), new StringCollection(['services.xml'])),
-]);
-
-$container = $loader->load((new OrchestratorBuilder($orchestrators))->build());
+$container = $loader->load($fromFiles);
 var_dump(getenv('ADMIN_APPLICATION_URL'));
 echo "Check some environment variables ...\n\n";
 
